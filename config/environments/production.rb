34277cd1298aa47be12af22a9a3901af3b362f57
Hilderlan <hilakt9698@gmail.com>
2021-05-09 18:39:59 -0300

    feat: Settings for deploying on heroku

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,3 +1,4 @@
+# rubocop: disable Metrics/BlockLength
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
 
@@ -82,4 +83,19 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+  # Email
+  config.action_mailer.default_url_options = {
+    host: 'blog-devise-token-auth-api.herokuapp.com'
+  }
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    address: 'smtp.gmail.com',
+    port: 587,
+    domain: 'gmail.com',
+    user_name: Rails.application.credentials.gmail[:user_name].to_s,
+    password: Rails.application.credentials.gmail[:password].to_s,
+    authentication: 'plain',
+    enable_starttls_auto: true
+  }
 end
